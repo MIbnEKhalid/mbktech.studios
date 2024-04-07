9bb16190a0e3d948f6dffef5086a161011554c21
Muhammad-Ibn-E-Khalid <83428194+Muhammad-Ibn-E-Khalid@users.noreply.github.com>
2024-04-07 12:04:11 +0500

    Update Update_Social_links.js

--- Update_Social_links.js
+++ Update_Social_links.js
@@ -6,14 +6,18 @@ function updateSocialLinks() {
     links.forEach(function(link) {
         var href = link.getAttribute('href');
         // Update the href attribute based on your requirement
-        if (href.includes('youtube.com')) {
-            link.setAttribute('href', 'youtube.com/@MBKTechStudios');
+        if (href.includes('gmail.com')) {
+            link.setAttribute('href', 'https://gmail.com');
+        } else if (href.includes('youtube.com')) {
+            link.setAttribute('href', 'https://youtube.com/@MBKTechStudios');
+        } else if (href.includes('github.com')) {
+            link.setAttribute('href', 'https://github.com/Muhammad-Ibn-E-Khalid');
         } else if (href.includes('facebook.com')) {
-            link.setAttribute('href', 'facebook.com/@MBKTechStudios');
+            link.setAttribute('href', 'https://facebook.com/@MBKTechStudios');
         } else if (href.includes('discord.com')) {
-            link.setAttribute('href', 'discord.gg/MUAyAeckrM');
+            link.setAttribute('href', 'https://discord.gg/MUAyAeckrM');
         } else if (href.includes('instagram.com')) {
-            link.setAttribute('href', 'instagram.com/mbktech.studios');
+            link.setAttribute('href', 'https://instagram.com/mbktech.studios');
         }
     });
 }
